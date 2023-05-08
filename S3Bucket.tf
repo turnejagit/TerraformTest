c941fba1d867e8f9d6830faf54826723e4931be5
turnejagit <130702520+turnejagit@users.noreply.github.com>
2023-05-08 15:41:43 -0500

    Delete bucket

--- S3Bucket.tf
+++ S3Bucket.tf
@@ -1,12 +1,13 @@
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
-resource "aws_s3_bucket" "example" {
-  bucket = "jat1-jat-my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#resource "aws_s3_bucket" "example" {
+#  bucket = "jat1-jat-my-tf-test-bucket"
+#
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#
 }
