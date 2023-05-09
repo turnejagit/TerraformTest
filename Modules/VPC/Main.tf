f363b964b9cee63bb7e72bca26e58e624769c73b
turnejagit <130702520+turnejagit@users.noreply.github.com>
2023-05-09 10:25:15 -0500

    add dns enabled

--- Modules/VPC/Main.tf
+++ Modules/VPC/Main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 resource "aws_vpc" "Terraform" {
   cidr_block       = "10.10.0.0/16"
+  enable_dns_support = “true” 
+  enable_dns_hostnames = “true” 
   instance_tenancy = "default"
 
   tags = {
