3df0b13d690ac227e3ee077188316d22bc595e8f
turnejagit <130702520+turnejagit@users.noreply.github.com>
2023-05-09 10:28:57 -0500

    add subnet

--- Modules/VPC/Main.tf
+++ Modules/VPC/Main.tf
@@ -4,11 +4,21 @@ provider "aws" {
 
 resource "aws_vpc" "Terraform" {
   cidr_block       = "10.10.0.0/16"
-  enable_dns_support = “true” 
-  enable_dns_hostnames = “true” 
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
   instance_tenancy = "default"
 
   tags = {
     Name = "Terraform Created"
   }
+}
+
+resource "aws_subnet" "private subnet 1" {
+    vpc_id = "${aws_vpc.Terraform.id}"
+    cidr_block = "10.10.0.0/24"
+    # map_public_ip_on_launch = “true” //it makes this a public subnet
+    availability_zone = "us-east-1a"
+    tags {
+        Name = "private subnet 1"
+    }
 }
