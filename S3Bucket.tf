668f53b320d4b8412f00cf8615eeb67a744c9697
turnejagit <130702520+turnejagit@users.noreply.github.com>
2023-05-09 09:54:22 -0500

    fix dual region in original bucket

--- S3Bucket.tf
+++ S3Bucket.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#resource "aws_s3_bucket" "example" {
-#  bucket = "jat1-jat-my-tf-test-bucket"
-#
-#  tags = {
-#    Name        = "My bucket"
-#    Environment = "Dev"
-#  }
-#
-#}
-
-provider "aws" {
-  region = "us-east-2"
-}
 
 resource "aws_s3_bucket" "example" {
   bucket = "jat1-jat-my-tf-test-bucket"
