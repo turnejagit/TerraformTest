7c36036b6d42cded72469df3248ed43681c4f0d5
turnejagit <130702520+turnejagit@users.noreply.github.com>
2023-05-09 09:48:35 -0500

    fix dual region

--- Modules/S3/Main.tf
+++ Modules/S3/Main.tf
@@ -1,18 +1,4 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-#resource "aws_s3_bucket" "example" {
-#  bucket = "jat1-jat-my-tf-test-bucket"
-#
-#  tags = {
-#    Name        = "My bucket"
-#    Environment = "Dev"
-#  }
-#
-#}
-
-provider "aws" {
   region = "us-east-2"
 }
 
