76de6e0efdda82007d88644e1104f8dae43d337a
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 14:44:50 -0500

    Test

--- Modules/S3test/Variables.tf
+++ Modules/S3test/Variables.tf
@@ -1,23 +1,10 @@
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-    default     = "testforjeffturner"
+variable "bucket_name" {
+  description = "Name of the s3 bucket. Must be unique."
+  type = string
 }
-variable "acl" {
-    type        = string
-    description = "(Optional) The canned ACL to apply. Defaults to private. Conflicts with grant."
-    default     = "private"
-}
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
+
 variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "prod"
-        terraform   = "true"
-    }
+  description = "Tags to set on the bucket."
+  type = map(string)
+  default = {}
 }
