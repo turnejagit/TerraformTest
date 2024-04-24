030c8c8dd6d5e4d58f394806c0f883c611322eab
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 08:09:45 -0500

    Change bucket

--- S3Bucket.tf
+++ S3Bucket.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "jat1-jat-my-tf-test-bucket"
+  bucket = "jat1-jat-my-tf-test-bucket-2"
 
   tags = {
     Name        = "My bucket"
