bb0350fa5cc415501b50973c780e0ef57a09c821
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 13:53:53 -0500

    new

--- Main.tf
+++ Main.tf
@@ -9,3 +9,8 @@ source = "git::https://@github.com/turnejagit/TerraformTest//Modules/S3"
 module "VPC" {
 source = "git::https://@github.com/turnejagit/TerraformTest//Modules/VPC"
 }
+
+module "Bucket1" {
+source = "git::https://@github.com/turnejagit/TerraformTest//Modules/S3test"
+s3-bucket-name = "jat-test-module-bucket"
+}
