f306b56e1b3b68e203805ae533b5518a11213396
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 13:19:22 -0500

    new

--- S3Bucket.tf
+++ S3Bucket.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
 
-resource "aws_s3_bucket" "example" {
-  bucket = "jat1-jat-my-tf-test-bucket-2"
+#resource "aws_s3_bucket" "example" {
+#  bucket = "jat1-jat-my-tf-test-bucket-2"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
 
-}
+#}
