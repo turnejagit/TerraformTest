9d06247e07a4e838b24977dece7a28ed4c7b55d1
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 12:52:32 -0500

    cleanup

--- Main.tf
+++
@@ -1,7 +0,0 @@
-module "S3" {
-source = "git::https://@github.com/turnejagit/TerraformTest//Modules/S3"
-}
-
-module "VPC" {
-source = "git::https://@github.com/turnejagit/TerraformTest//Modules/VPC"
-}
