2fe804a392386a956a385d488d8ab9d37b20c8a3
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 14:52:39 -0500

    fix

--- JT-Prod-Buckets/Main.tf
+++ JT-Prod-Buckets/Main.tf
@@ -4,5 +4,5 @@ provider "aws" {
 
 module "Bucket1" {
 source = "git::https://@github.com/turnejagit/TerraformTest//Modules/S3test"
-s3-bucket-name = "jat-test-module-bucket"
+bucket_name = "jat-test-module-bucket"
 }
