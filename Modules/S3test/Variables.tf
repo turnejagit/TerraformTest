281d3748a45da5bc04e862821ba111ea5c40d57d
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 13:30:00 -0500

    1

--- Modules/S3test/Variables.tf
+++ Modules/S3test/Variables.tf
@@ -1,15 +1,23 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = "testforjeffturner"
 }
-
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "acl" {
+    type        = string
+    description = "(Optional) The canned ACL to apply. Defaults to private. Conflicts with grant."
+    default     = "private"
 }
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
 }
-
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "prod"
+        terraform   = "true"
+    }
+}
