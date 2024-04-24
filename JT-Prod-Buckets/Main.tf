31d1a0d0a0faafd436a4cc4ee8d61a8e5dd39947
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 14:56:16 -0500

    fixes2

--- JT-Prod-Buckets/Main.tf
+++ JT-Prod-Buckets/Main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "Bucket1" {
+module "Bucket2" {
 source = "git::https://@github.com/turnejagit/TerraformTest//Modules/S3test"
 bucket_name = "jat-test-module-bucket"
 }
