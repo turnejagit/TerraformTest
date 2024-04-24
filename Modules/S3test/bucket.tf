40b6c3ca8bad6b73382029da6935b9d83f6c534a
Jeff Turner <turneja@Jeffs-MacBook-Pro.local>
2024-04-24 12:24:41 -0500

    fix

--- Modules/S3test/bucket.tf
+++ Modules/S3test/bucket.tf
@@ -1,13 +1,4 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "jat2-jat-my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
+resource "aws_s3_bucket" "demos3" {
+    bucket = "${var.bucket_name}" 
+    acl = "${var.acl_value}"   
 }
